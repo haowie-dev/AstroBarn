f5132064109309c56f6b873eb9027f9e28247b7c
Jamie <jamie.liu386@gmail.com>
2019-02-16 15:15:12 -0800

    add code to render cow

--- main-scene.js
+++ main-scene.js
@@ -86,17 +86,159 @@ class Assignment_Two_Skeleton extends Scene_Component {
             this.t += graphics_state.animation_delta_time / 1000;
         const t = this.t;
 
+        let m = Mat4.identity();
+
+        this.draw_cow(graphics_state, m);
+
+//         m = m.times(Mat4.translation(Vec.of(-3 * t, 0, 0)));
+        
         // Draw some demo textured shapes
-        let spacing = 6;
-        let m = Mat4.translation(Vec.of(-1 * (spacing / 2) * (this.shape_count - 1), 0, 0));
-        for (let k in this.shapes) {
-            this.shapes[k].draw(
-                graphics_state,
-                m.times(Mat4.rotation(t, Vec.of(0, 1, 0))),
-                this.shape_materials[k] || this.plastic);
-            m = m.times(Mat4.translation(Vec.of(spacing, 0, 0)));
-        }
+//         let spacing = 6;
+//         let m = Mat4.translation(Vec.of(-1 * (spacing / 2) * (this.shape_count - 1), 0, 0));
+//         for (let k in this.shapes) {
+//             this.shapes[k].draw(
+//                 graphics_state,
+//                 m.times(Mat4.rotation(t, Vec.of(0, 1, 0))),
+//                 this.shape_materials[k] || this.plastic);
+//             m = m.times(Mat4.translation(Vec.of(spacing, 0, 0)));
+//         }
     }
+        draw_cow(graphics_state, m) {
+
+            this.draw_body(graphics_state, m);   
+
+            this.draw_legs(graphics_state, m, 1, 1);
+            this.draw_legs(graphics_state, m, -1, 1);  
+
+            this.draw_legs(graphics_state, m, 1, -1);
+            this.draw_legs(graphics_state, m, -1, -1);
+
+            this.draw_tail(graphics_state, m);
+
+            this.draw_head(graphics_state, m);
+        }
+
+        draw_body(graphics_state,m) {
+            this.shapes.cylinder.draw( // main body
+                graphics_state,
+                m
+                 .times(Mat4.rotation(Math.PI/2, Vec.of(0,1,0)))
+                 .times(Mat4.scale(Vec.of(4,4,4))),
+                this.clay)
+            this.shapes.ball.draw( // front sphere
+                graphics_state,
+                m   
+                 .times(Mat4.translation(Vec.of(-4, 0, 0)))
+                 .times(Mat4.scale(Vec.of(3,4,4))),
+                this.clay)
+            this.shapes.ball.draw( // back sphere
+                graphics_state,
+                m   
+                 .times(Mat4.translation(Vec.of(4, 0, 0)))
+                 .times(Mat4.scale(Vec.of(3,4,4))),
+                this.clay)
+        }
+
+        draw_legs(graphics_state, m, side, fb) {
+            const deg = -.3 + Math.sin(side * 3.2 * this.t + (-Math.PI/4 * fb * side))
+            m = m.times(Mat4.translation(Vec.of(fb * -4.3, -Math.sqrt(8), side * Math.sqrt(8)))) // move system to first joint connection
+                 .times(Mat4.rotation(0.3 * deg, Vec.of(0,0,1)))
+            this.shapes.ball.draw( // first joint
+                graphics_state, 
+                m.times(Mat4.scale(1.3)),
+                this.clay)
+            this.shapes.cylinder.draw( // draw first leg section
+                graphics_state,
+                m
+                 .times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0)))
+                 .times(Mat4.translation(Vec.of(0,0,1.3)))
+                 .times(Mat4.scale(1.3))
+                ,
+                this.clay)
+            m = m.times(Mat4.translation(Vec.of(0,-2.6,0))) // move system to second leg joint
+                 .times(Mat4.rotation(-0.4 * deg, Vec.of(0,0,1)))
+            this.shapes.ball.draw(
+                graphics_state, 
+                m.times(Mat4.scale(1.3)), 
+                this.clay)
+            this.shapes.cylinder.draw( // draw second leg section
+                graphics_state,
+                m
+                 .times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0)))
+                 .times(Mat4.translation(Vec.of(0,0,1.3)))
+                 .times(Mat4.scale(Vec.of(1.3,1.3,1.3)))
+                ,
+                this.clay)
+            m = m.times(Mat4.translation(Vec.of(0,-2.6,0))) // move to third joint
+                 .times(Mat4.rotation( 0.1 * deg, Vec.of(0,0,1)))
+            this.shapes.ball.draw(
+                graphics_state,
+                m.times(Mat4.scale(1.3)),
+                this.clay)
+            this.shapes.box.draw( // draw foot
+                graphics_state,
+                m.times(Mat4.translation(Vec.of(0, -.75, 0)))
+                 .times(Mat4.scale(Vec.of(1.3,.8,1.3))),
+                this.clay)
+        }
+
+        draw_tail(graphics_state, m) {
+            const deg = Math.sin(this.t);
+            let sign = Math.sign(deg);
+             m = m.times(Mat4.translation(Vec.of(4, 0, 0))) // translate to middle of ellipsoid
+                  .times(Mat4.rotation(-Math.PI/4, Vec.of(0,0,1))) // rotate
+                  .times(Mat4.translation(Vec.of(-1, .15 + Math.sqrt(25/2),0))) // translate to edge of ellipsoid
+            this.shapes.box.draw( // first box
+                graphics_state,
+                m.times(Mat4.scale(.15)),
+                this.clay );
+            for (let i = 0; i < 30; i++) {
+                if (i < 9) {
+                    m = m.times(Mat4.translation(Vec.of(.15, .15, sign * .15))) // translate axis of rotation
+                         .times(Mat4.rotation(-3 * (Math.PI/4) / 9,Vec.of(0,0, 1))) // apply tail curve rotation
+                         .times(Mat4.rotation(.01 * deg, Vec.of(1,0, 0))) // apply swinging rotation
+                         .times(Mat4.translation(Vec.of(-.15, .15,sign * -.15))); // translate back
+                }
+                else {
+                    m = m.times(Mat4.translation(Vec.of(0, .15, sign * .15)))
+                         .times(Mat4.rotation(.1 * deg, Vec.of(1,0, 0)))
+                         .times(Mat4.translation(Vec.of(0, .15, sign * -.15)));
+                }
+                this.shapes.box.draw(
+                    graphics_state,
+                    m.times(Mat4.scale(.15)),
+                    this.clay )
+            }   
+        }
+
+        draw_head(graphics_state, m) {
+            m = m.times(Mat4.rotation(-Math.PI/2, Vec.of(0,1,0)))
+                 .times(Mat4.translation(Vec.of(0,0,4)))
+                 .times(Mat4.rotation(-Math.PI/4, Vec.of(1,0,0)))
+                 .times(Mat4.translation(Vec.of(0, 0, Math.sqrt(25/2))));
+            this.shapes.cylinder.draw(
+                graphics_state,
+                m.times(Mat4.scale(Vec.of(1.5,1.5,1))),
+                this.clay )
+            m = m.times(Mat4.translation(Vec.of(0, -.8, 2.2)))
+            this.shapes.ball.draw(
+                graphics_state,
+                m.times(Mat4.scale(Vec.of(3,4,2.5)))
+                 .times(Mat4.rotation(-Math.PI/4, Vec.of(1,0,0)))
+                ,
+                this.clay
+                )
+            this.shapes.ball.draw(
+                graphics_state,
+                m.times(Mat4.scale(Vec.of(1.7, .8, .3)))
+                 .times(Mat4.translation(Vec.of(-2.4, 1.4, 0))),
+                this.clay )
+            this.shapes.ball.draw(
+                graphics_state,
+                m.times(Mat4.scale(Vec.of(1.7, .8, .3)))
+                 .times(Mat4.translation(Vec.of(2.4, 1.4, 0))),
+                this.clay )
+        }
 }
 
 window.Assignment_Two_Skeleton = window.classes.Assignment_Two_Skeleton = Assignment_Two_Skeleton;
