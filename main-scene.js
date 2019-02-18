26536d47cd11cc2558251ca2176898d607db767c
Hao Nguyen <haolnguyen7@gmail.com>
2019-02-18 00:03:04 -0800

    barny

--- main-scene.js
+++ main-scene.js
@@ -38,8 +38,8 @@ class Assignment_Two_Skeleton extends Scene_Component {
       box: new Cube(),
       cylinder: new Cylinder(15),
       cone: new Cone(20),
-      ball: new Subdivision_Sphere(4)
-      //   triangular_prism: new TriangularPrism()
+      ball: new Subdivision_Sphere(4),
+      triangular_prism: new TriangularPrism()
     };
     this.submit_shapes(context, shapes);
     this.shape_count = Object.keys(shapes).length;
@@ -61,6 +61,12 @@ class Assignment_Two_Skeleton extends Scene_Component {
         diffusivity: 0.4,
         specularity: 0.3
       });
+
+    // colors
+    this.blue = Color.of(0, 0, 1, 1);
+    this.yellow = Color.of(1, 1, 0, 1);
+    this.brown = Color.of(165 / 255, 42 / 255, 42 / 255, 1);
+    this.brick = Color.of(178 / 255, 34 / 255, 34 / 255, 1);
 
     // Load some textures for the demo shapes
     this.shape_materials = {};
@@ -291,7 +297,9 @@ class Assignment_Two_Skeleton extends Scene_Component {
     this.shapes.box.draw(
       graphics_state,
       m.times(Mat4.scale(Vec.of(barn_length, barn_length, barn_width))),
-      this.plastic
+      this.plastic.override({
+        color: this.brick
+      })
     );
     m = m
       .times(Mat4.translation(Vec.of(-barn_length, 0, 0)))
@@ -300,7 +308,9 @@ class Assignment_Two_Skeleton extends Scene_Component {
     this.shapes.box.draw(
       graphics_state,
       m.times(Mat4.scale(Vec.of(barn_length, barn_length, barn_width))),
-      this.plastic
+      this.plastic.override({
+        color: this.brick
+      })
     );
     m = m
       .times(Mat4.translation(Vec.of(barn_length, 0, 0)))
@@ -309,7 +319,9 @@ class Assignment_Two_Skeleton extends Scene_Component {
     this.shapes.box.draw(
       graphics_state,
       m.times(Mat4.scale(Vec.of(barn_length, barn_length, barn_width))),
-      this.plastic
+      this.plastic.override({
+        color: this.brick
+      })
     );
     m = m
       .times(Mat4.translation(Vec.of(-barn_length, 0, 0)))
@@ -322,23 +334,95 @@ class Assignment_Two_Skeleton extends Scene_Component {
     this.shapes.box.draw(
       graphics_state,
       m.times(Mat4.scale(Vec.of(barn_length / 4, barn_length, barn_width))),
-      this.plastic
+      this.plastic.override({
+        color: this.brick
+      })
     );
     m = m.times(Mat4.translation(Vec.of((3 * barn_length) / 2, 0, 0)));
     this.shapes.box.draw(
       graphics_state,
       m.times(Mat4.scale(Vec.of(barn_length / 4, barn_length, barn_width))),
-      this.plastic
+      this.plastic.override({
+        color: this.brick
+      })
     );
   }
 
   draw_barn_roof(graphics_state, m) {
     const barn_length = 20;
-    // this.shapes.triangular_prism.draw(
-    //   graphics_state,
-    //   m.times(Mat4.scale(Vec.of(barn_length, barn_length, barn_length))),
-    //   this.plastic
-    // );
+    const barn_width = 1;
+    const barn_roof_length = barn_length / Math.sqrt(2) + barn_width;
+    // m = m.times(Mat4.rotation(Math.PI / 2, Vec.of(0, -1, 0)));
+
+    this.shapes.box.draw(
+      graphics_state,
+      m
+        .times(Mat4.rotation(Math.PI / 2, Vec.of(0, -1, 0)))
+        .times(Mat4.translation(Vec.of(0, 30, -10)))
+        .times(Mat4.rotation(-Math.PI / 4, Vec.of(1, 0, 0)))
+        .times(
+          Mat4.scale(Vec.of(barn_length + 4, barn_roof_length, barn_width))
+        ),
+      this.plastic.override({
+        color: this.brick
+      })
+    );
+
+    this.shapes.box.draw(
+      graphics_state,
+      m
+        .times(Mat4.rotation(-Math.PI / 2, Vec.of(0, -1, 0)))
+        .times(Mat4.translation(Vec.of(0, 30, 2 * barn_length - 10)))
+        .times(Mat4.rotation(-Math.PI / 4, Vec.of(1, 0, 0)))
+        .times(
+          Mat4.scale(Vec.of(barn_length + 4, barn_roof_length, barn_width))
+        ),
+      this.plastic.override({
+        color: this.brick
+      })
+    );
+
+    this.shapes.triangular_prism.draw(
+      graphics_state,
+      m
+        .times(
+          Mat4.translation(
+            Vec.of(
+              barn_length,
+              barn_length + barn_length / Math.sqrt(2) + 5,
+              barn_length - barn_width
+            )
+          )
+        )
+        .times(Mat4.rotation((-3 * Math.PI) / 4, Vec.of(0, 0, 1)))
+        .times(
+          Mat4.scale(Vec.of(barn_length + 8, barn_length + 8, barn_width))
+        ),
+      this.plastic.override({
+        color: this.brick
+      })
+    );
+
+    this.shapes.triangular_prism.draw(
+      graphics_state,
+      m
+        .times(
+          Mat4.translation(
+            Vec.of(
+              barn_length,
+              barn_length + barn_length / Math.sqrt(2) + 5,
+              -barn_length - barn_width / Math.sqrt(2)
+            )
+          )
+        )
+        .times(Mat4.rotation((-3 * Math.PI) / 4, Vec.of(0, 0, 1)))
+        .times(
+          Mat4.scale(Vec.of(barn_length + 8, barn_length + 8, barn_width))
+        ),
+      this.plastic.override({
+        color: this.brick
+      })
+    );
   }
 }
 
